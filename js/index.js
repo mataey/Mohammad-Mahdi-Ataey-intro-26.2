4e5e3c863fb22ff207e2033c60a69514e71f55fb
Mahdi Ataey <mahdiataey@macbookpro.mynetworksettings.com>
2026-05-26 19:53:21 -0400

    complete lesson-8 assignment with message form and styles

--- js/index.js
+++ js/index.js
@@ -20,3 +20,32 @@ for(let i = 0 ; i < skills.length; i++){
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> <span>wrote: ${message}</span> `;
+    
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', function(){
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    
+    messageForm.reset();
+});
